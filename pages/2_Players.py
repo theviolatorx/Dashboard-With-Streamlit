c4f2a87210c728d1bb7f132c7fa28ea6c72ccefc
Clovis Garcia <theviolatorx@gmail.com>
2023-07-21 00:00:23 -0300

    Primeiro Commit

---
+++ pages/2_Players.py
@@ -0,0 +1,39 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='Players',
+    page_icon="🏃‍♂️",
+    layout='wide',
+)
+
+df_data = st.session_state['data']
+
+clubes = df_data['Club'].value_counts().index
+club = st.sidebar.selectbox("Clubes", clubes)
+
+df_player = df_data[df_data["Club"] == club]
+players = df_player['Name'].value_counts().index
+player = st.sidebar.selectbox("Jogadores", players)
+
+player_stats = df_data[df_data['Name'] == player].iloc[0]
+
+st.image(player_stats['Photo'])
+st.title(f' {player_stats["Name"]}')
+
+st.markdown(f'**Clube:** {player_stats["Club"]}')
+st.markdown(f'**Clube:** {player_stats["Position"]}')
+
+col1, col2, col3, col4 = st.columns(4)
+col1.markdown(f'**Idade:** {player_stats["Age"]}')
+col2.markdown(f'**Altura:** {player_stats["Height(cm.)"]/100}')
+col3.markdown(f'**Peso:** {player_stats["Weight(lbs.)"] * 0.453:.2f}  ')
+
+st.divider()
+st.subheader(f'Overall {player_stats["Overall"]}')
+st.progress(int(player_stats["Overall"]))
+
+col1, col2, col3, col4 = st.columns(4)
+col1.metric('Valor de Mercado', value=f'£ {player_stats["Value(£)"]:,.2f}')
+col2.metric('Remuneração Semanal', value=f'£ {player_stats["Wage(£)"]:,.2f}')
+col3.metric('Claúsula de Rescisão',
+            value=f'£ {player_stats["Release Clause(£)"]:,.2f}')
